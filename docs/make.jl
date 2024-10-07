9a1d71a200bc146fdea1e1ae6859d1426873890d
Abel Soares Siqueira <abel.siqueira@esciencecenter.nl>
2024-10-07 14:33:16 +0200

    Manufally fix remaining abelsiqueira references

--- docs/make.jl
+++ docs/make.jl
@@ -12,10 +12,10 @@ const numbered_pages = [
 makedocs(;
     modules = [BestieMinimum],
     authors = "Abel Soares Siqueira <abel.s.siqueira@gmail.com> and contributors",
-    repo = "https://github.com/abelsiqueira/BestieMinimum.jl/blob/{commit}{path}#{line}",
+    repo = "https://github.com/JuliaBesties/BestieMinimum.jl/blob/{commit}{path}#{line}",
     sitename = "BestieMinimum.jl",
-    format = Documenter.HTML(; canonical = "https://abelsiqueira.github.io/BestieMinimum.jl"),
+    format = Documenter.HTML(; canonical = "https://JuliaBesties.github.io/BestieMinimum.jl"),
     pages = ["index.md"; numbered_pages],
 )
 
-deploydocs(; repo = "github.com/abelsiqueira/BestieMinimum.jl")
+deploydocs(; repo = "github.com/JuliaBesties/BestieMinimum.jl")
